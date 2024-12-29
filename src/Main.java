b20601699fa1f644b569d30f3af4665178298454
Пололин Сергей Александрович <spololin@sportmaster.ru>
2024-12-30 00:21:55 +0300

    Реализовал считывание данных файла

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+
+    public static void main(String[] args) throws IOException {
+        List fileLines = getFileLines();
+
+        System.out.println("Number of lines: " + fileLines.size());
+    }
+
+
+    private static List getFileLines() throws IOException {
+        final String COMMA_DELIMITER = ",";
+        List records = new ArrayList();
+
+        BufferedReader br = new BufferedReader(new FileReader("static/files/NetflixOriginals.csv"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] values = line.split(COMMA_DELIMITER);
+            records.add(Arrays.asList(values));
+        }
+
+        return records;
     }
 }
